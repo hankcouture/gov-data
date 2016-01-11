597793a0d1b56f82595d8ea89cbb6a6be3b96b44
Hank Couture <hankcouture@gmail.com>
2016-01-11 15:00:16 -0600

    added two charts on weekly wage and hours data

--- public/app/controllers/HomeController.js
+++ public/app/controllers/HomeController.js
@@ -2,25 +2,62 @@ angular.module("home", ["chart.js"]).controller("HomeCtrl", function ($scope, $h
 
   // Temporary Data Storage
 
-  $scope.labels = [];
-  $scope.series = ['US Unemployment Rate'];
-  $scope.data = [
-    []
-  ];
+  $scope.unemploymentData = {}
+  $scope.unemploymentData.series = ['US Unemployment Rate'];
+  $scope.unemploymentData.labels = undefined;
+  $scope.unemploymentData.data = [[]];
+  $scope.wagesData = {}
+  $scope.wagesData.series = ['Avg. Hourly Pay per Worker'];
+  $scope.wagesData.labels = undefined;
+  $scope.wagesData.data = [[]];
+  $scope.hoursData = {}
+  $scope.hoursData.series = ['Avg. Weekly Hours per Worker'];
+  $scope.hoursData.labels = undefined;
+  $scope.hoursData.data = [[]];
+
   $scope.onClick = function (points, evt) {
     console.log(points, evt);
   };
 
-  $scope.getData = function() {
+  $scope.getUnemploymentData = function() {
     $http({
       method: 'GET',
       headers: 'application/json',
-      url: '/data'
+      url: '/unemploymentData'
     }).then(function(res) {
-      $scope.labels = res.data.labelsArr;
-      $scope.data[0] = res.data.dataArr;
-      console.log($scope.data)
+      $scope.unemploymentData.labels = res.data.labelsArr;
+      $scope.unemploymentData.data[0] = res.data.dataArr;
     })
+  }
+
+  $scope.getHoursData = function() {
+    $http({
+      method: 'GET',
+      headers: 'application/json',
+      url: '/hoursData'
+    }).then(function(res) {
+      $scope.hoursData.labels = res.data.labelsArr;
+      $scope.hoursData.data[0] = res.data.dataArr;
+      console.log(res.data)
+    })
+  }
+
+  $scope.getWagesData = function() {
+    $http({
+      method: 'GET',
+      headers: 'application/json',
+      url: '/wagesData'
+    }).then(function(res) {
+      $scope.wagesData.labels = res.data.labelsArr;
+      $scope.wagesData.data[0] = res.data.dataArr;
+      console.log(res.data)
+    })
+  }
+
+  $scope.getData = function() {
+    $scope.getUnemploymentData();
+    $scope.getHoursData();
+    $scope.getWagesData();
   }
 
   $scope.getData();
--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/data', function(req, res){
+app.get('/unemploymentData', function(req, res){
+	console.log(req)
 	requestify.post('http://api.bls.gov/publicAPI/v2/timeseries/data/', {
 		seriesid: ["LNS14000000"],
 		registrationKey: tokens.gov.key
@@ -16,7 +17,6 @@ app.get('/data', function(req, res){
 	  .then(function(response) {
 	  	// Get the response body (JSON parsed or jQuery object for XMLs)
 	  	var data = response.getBody();
-	  	console.log(data)
 	  	var good = JSON.parse(response.getBody());
 	  	var result = good.Results.series[0];
 	  	var obj = {}
@@ -28,7 +28,58 @@ app.get('/data', function(req, res){
 	  	}
 	  	obj.labelsArr.reverse();
 	  	obj.dataArr.reverse();
-	  	console.log(obj)
+    	res.setHeader('Content-Type', "application/json");
+    	res.status(200).send(obj);
+	  }
+	);
+});
+
+app.get('/hoursData', function(req, res){
+	console.log(req)
+	requestify.post('http://api.bls.gov/publicAPI/v2/timeseries/data/', {
+		seriesid: ["CES0500000002"],
+		registrationKey: tokens.gov.key
+	})
+	  .then(function(response) {
+	  	// Get the response body (JSON parsed or jQuery object for XMLs)
+	  	var data = response.getBody();
+	  	var good = JSON.parse(response.getBody());
+	  	var result = good.Results.series[0];
+	  	var obj = {}
+	  	obj.labelsArr = [];
+	  	obj.dataArr = [];
+	  	for (var i = 0; i < result.data.length; i++) {
+	  		obj.labelsArr.push(result.data[i].periodName + ' ' + result.data[i].year)
+	  		obj.dataArr.push(result.data[i].value)
+	  	}
+	  	obj.labelsArr.reverse();
+	  	obj.dataArr.reverse();
+    	res.setHeader('Content-Type', "application/json");
+    	res.status(200).send(obj);
+	  }
+	);
+});
+
+app.get('/wagesData', function(req, res){
+	console.log(req)
+	requestify.post('http://api.bls.gov/publicAPI/v2/timeseries/data/', {
+		seriesid: ["CES0500000003"],
+		registrationKey: tokens.gov.key
+	})
+	  .then(function(response) {
+	  	// Get the response body (JSON parsed or jQuery object for XMLs)
+	  	var data = response.getBody();
+	  	var good = JSON.parse(response.getBody());
+	  	var result = good.Results.series[0];
+	  	var obj = {}
+	  	obj.labelsArr = [];
+	  	obj.dataArr = [];
+	  	for (var i = 0; i < result.data.length; i++) {
+	  		obj.labelsArr.push(result.data[i].periodName + ' ' + result.data[i].year)
+	  		obj.dataArr.push(result.data[i].value)
+	  	}
+	  	obj.labelsArr.reverse();
+	  	obj.dataArr.reverse();
     	res.setHeader('Content-Type', "application/json");
     	res.status(200).send(obj);
 	  }
